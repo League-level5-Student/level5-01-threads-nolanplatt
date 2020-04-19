83e81c1bdc49372f973ae978a6694d5180ec37eb
Nolan Platt <nolanplatt@Direct.san.rr.com>
2020-04-19 14:16:41 -0700

    module 1 done
    module 1 done

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,39 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
 
-}
+	Thread[] thread;
+	ConcurrentLinkedQueue<Task> taskQueue;
+	
+	
+	ThreadPool(int totalThreads) {
+		thread = new Thread[totalThreads];
+		
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+		
+		for (int i = 0; i < thread.length; i++) {
+			thread[i] = new Thread(new Worker(taskQueue));
+		}
+		
+	}
+	
+	void addTask(Task task) {
+		taskQueue.add(task);
+	}
+	
+	void start() {
+		for (int i = 0; i < thread.length; i++) {
+			thread[i].start();
+			try {
+				thread[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	}
+	
+
